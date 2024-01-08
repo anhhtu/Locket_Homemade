ff5979206c0621ce1522b3ae3f70f7534f97e4de
Vũ Anh Tú <vuanhtu0711@gmail.com>
2024-01-08 14:59:37 +0700

    Initial Commit

---
+++ Locket_Homemade/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Locket_Homemade
+//
+//  Created by Vũ Anh Tú on 08/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Locket_Homemade/Locket_HomemadeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Locket_HomemadeApp.swift
+//  Locket_Homemade
+//
+//  Created by Vũ Anh Tú on 08/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Locket_HomemadeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
